079260d0fe4c231b2453eb7f7541cd2ca399fe94
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 02:39:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  array = [1, 2, 3]
-array.each do |num|
-  puts num*num
+  
+new_numbers = []
+end 	  numbers.each do |num|
+    new_numbers << (num ** 2)
   end
+  return new_numbers
 end
